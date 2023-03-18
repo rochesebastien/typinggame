6f040817b0338cab29de91f929d56e8a446117b8
Roche Sébastien <sebastienroche230@gmail.com>
2023-03-18 20:18:55 +0100

    First commit : start of the project

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+const test = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nulla rem possimus ipsa cum nemo perferendis molestias culpa architecto eius fugit provident, autem, nesciunt, ea obcaecati aliquid? Labore sapiente odio at aut suscipit nam est hic eum amet facere perferendis sequi culpa atque, blanditiis error itaque soluta debitis pariatur expedita, saepe nostrum quisquam voluptatibus ratione reiciendis? Magni maxime veritatis atque? Culpa laborum error numquam ducimus autem. Ipsa architecto iure, repellendus, accusamus aliquam fuga totam harum saepe cum itaque libero eligendi ullam iusto repellat suscipit amet temporibus, optio neque corporis aperiam fugiat est corrupti ratione? Reiciendis est eaque autem repellat ducimus?"
+
+window.addEventListener('keydown', function (e) {
+    console.log(`You pressed ${e.key}`);
+  }, false);
